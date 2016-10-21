b083685b6ca9e596bfc19fb81e351af7eab1b2a1
Amanda Baker <Amanda Baker>
2016-10-21 12:30:20 -0400

    Basic Physics

---
+++ Assets/playerGravity.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerGravity : MonoBehaviour {
+
+	GameObject 		player;
+	GameObject 		sink;
+	GameObject[] 	sinks;
+	float 	G;         	// Gravitational constant: use for scaling
+	float 	M;          // Mass of sink: use for pull intensity
+	Vector3 r;          // Radius vector between two objects
+	float 	R;          // Magnitude of radius
+	float 	a;          // Magnitude of acceleration
+	Vector3 v;          // Velocity 
+	Vector3 dv;         // Change in velocity
+	float 	dt;        	// Time step
+
+	// Use this for initialization
+	void Start () {
+		G = 0.1F;
+		M = 100;
+		dt = 0.1F;
+
+		player = GameObject.FindWithTag("Player");
+		sinks = GameObject.FindGameObjectsWithTag("Sink");
+		sink = sinks [0];
+		// Player starts with an initial velocity
+		v = new Vector3(-1, 0, 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 A = new Vector3 (0, 0, 0);
+		foreach (GameObject sink in sinks) {
+			// radius between the two objects
+			r = sink.transform.position - player.transform.position;
+			// magnitude of radius
+			R = r.magnitude;
+
+			// magnitude of acceleration
+			a = G * M / R / R;
+			A += r / R * a;
+
+			// change in velocity
+			dv = dt * A;
+			// velocity
+			v += Vector3.Scale(dv, new Vector3(1,0,1));
+		}
+
+		// player's new position is the old position + the change in position
+		player.transform.position += v*dt;
+	}
+}
---
+++ Assets/pullToCylinder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pullToCylinder : MonoBehaviour {
+
+	GameObject player; 
+	GameObject sink;
+	float 	G;         	// Gravitational constant: use for scaling
+	float 	M;          // Mass of sink: use for pull intensity
+	Vector3 r;          // Radius vector between two objects
+	float 	R;          // Magnitude of radius
+	float 	a;          // Magnitude of acceleration
+	Vector3 v;          // Velocity 
+	Vector3 dv;         // Change in velocity
+	float 	dt;        	// Time step
+
+	// Use this for initialization
+	void Start () {
+		G = 0.1F;
+		M = 10;
+		dt = 0.1F;
+
+		player = GameObject.Find("Dynamic Sphere");
+		sink = GameObject.Find("Static Cylinder");
+
+		// Player starts with an initial velocity
+		v = new Vector3(-1, 0, 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// get the radius between the two objects
+		r = sink.transform.position - player.transform.position;
+		R = r.magnitude;
+
+		// Combine these equations to get magnitude of acceleration
+		//   F = G * M * m / R / R;
+		//   a = F / m;
+		a = G * M / R / R;
+
+		dv = r * a;
+		v += Vector3.Scale(dv, new Vector3(1,0,1));
+
+		player.transform.position += v*dt;
+	}
+}
