47686a1437ae7bc9f909eae25db6132144825a7e
ragnarok02 <martc86@gmail.com>
2016-11-29 15:47:14 -0500

    Timer and GameOver screens
    I added a timer and the GameOver scene in the build settings

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text score;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		
+	}
+
+	/**
+	 * Check this function with physsics
+	 * Score system?
+	 */
+	void OnTriggerEnter(Collider other) {
+		//check some collisions
+		//count += 50;
+	}
+
+	void SetScore() {
+		score.text = "Score: " + count.ToString ();
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	
+	public UIManager uimanager;
+	float timeLeft = 30.0f;
+	public Text timer;
+	// Use this for initialization
+	void Start () {
+		timer.text = ((int)timeLeft).ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		timer.text = ((int)timeLeft).ToString ();
+		if(timeLeft < 0) {
+			uimanager.LoadLevel ("GameOver");
+		}
+	}
+}
