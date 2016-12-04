43342921e240aa1e381f5b5b341322e23d0e6c20
Amanda Baker <amandaabaker21@gmail.com>
2016-12-04 11:22:36 -0500

    Comment out obselete function

--- Assets/Scripts/AnimatedQuad.cs
+++ Assets/Scripts/AnimatedQuad.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿/*
+using UnityEngine;
 using System.Collections;
  
 class AnimateTiledTexture : MonoBehaviour
@@ -39,3 +40,5 @@ class AnimateTiledTexture : MonoBehaviour
  
     }
 }
+
+*/
